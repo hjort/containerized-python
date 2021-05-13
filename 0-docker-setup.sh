2c9c51db43ab68f4830fa972a8949bdd932d3999
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2021-05-13 16:35:11 -0300

    0-docker-setup.sh

---
+++ 0-docker-setup.sh
@@ -0,0 +1,14 @@
+# instalação do Docker no Ubuntu
+
+apt show docker.io
+apt install docker.io
+
+systemctl status docker
+systemctl disable docker
+
+docker version
+
+systemctl stop docker
+systemctl start docker
+
+
