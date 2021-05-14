a8c319753e6b4ef3b227c82e088b45f3ebe1a0a9
Rodrigo Hjort <rodrigo.hjort@serpro.gov.br>
2021-05-14 10:47:30 -0300

    quebra de linha

--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-  return "Hello World in a Flask inside a Container!"
+  return "Hello World in a Flask inside a Container!\n"
 
 if __name__ == "__main__":
   server.run(host='0.0.0.0')
